8442a8d4975193e659e30c9369b230b4f308c5cf
xmarianF <xmarianF@gmail.com>
2016-11-10 00:07:10 +0100

    Add slider for bestsellers and novelties on dashboard.

---
+++ web/js/slider.js
@@ -0,0 +1,37 @@
+$(document).on('ready', function(){
+    $(".regular").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 512,
+                settings: {
+                    dots: true,
+                    infinite: true,
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 512,
+                settings: {
+                    dots: true,
+                    infinite: true,
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 256,
+                settings: {
+                    dots: true,
+                    infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            }
+        ]
+    });
+});
