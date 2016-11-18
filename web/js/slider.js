a141ac42d74352e77ea27b406a6d92ed2a53c2ac
piotrowiczmateusz <piotrowiczmateusz@onet.pl>
2016-11-18 14:47:30 +0100

    new theme v0.1

--- web/js/slider.js
+++ web/js/slider.js
@@ -2,34 +2,22 @@ $(document).on('ready', function(){
     $(".regular").slick({
         dots: false,
         infinite: true,
-        slidesToShow: 4,
+        slidesToShow: 3,
         slidesToScroll: 1,
         responsive: [
             {
-                breakpoint: 512,
+                breakpoint: 768,
                 settings: {
-                    dots: true,
-                    infinite: true,
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                }
-            },
-            {
-                breakpoint: 512,
-                settings: {
-                    dots: true,
-                    infinite: true,
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                }
-            },
-            {
-                breakpoint: 256,
-                settings: {
-                    dots: true,
-                    infinite: true,
                     slidesToShow: 2,
-                    slidesToScroll: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 460,
+                settings: {
+
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                 }
             }
         ]
